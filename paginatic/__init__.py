e83e17b45c224bc9aba14b4a8ff982526bd8153e
Allen Chou <f1470891079@gmail.com>
2026-05-01 13:11:37 +0800

    Fix typo in type variable name and enhance Paginatic model with default factory for data. Introduce TokenPaginatic class for paginated responses with a next page token.

--- paginatic/__init__.py
+++ paginatic/__init__.py
@@ -4,15 +4,25 @@ from pydantic import BaseModel, Field
 
 __version__: Final[Text] = "0.1.0"
 
-PydnaticModelT = TypeVar("PydnaticModelT", bound=BaseModel)
+PydanticModelT = TypeVar("PydanticModelT", bound=BaseModel)
 IdT = TypeVar("IdT")
 
 
-class Paginatic(BaseModel, Generic[PydnaticModelT, IdT]):
+class Paginatic(BaseModel, Generic[PydanticModelT, IdT]):
     object: Literal["list"] = Field(default="list", description="The type of object")
-    data: List[PydnaticModelT] = Field(..., description="The data of the list")
+    data: List[PydanticModelT] = Field(
+        default_factory=list, description="The data of the list"
+    )
     first_id: Optional[IdT] = Field(
         default=None, description="The first ID of the list"
     )
     last_id: Optional[IdT] = Field(default=None, description="The last ID of the list")
     has_more: bool = Field(default=False, description="Whether there are more items")
+
+
+class TokenPaginatic(BaseModel, Generic[PydanticModelT]):
+    object: Literal["list"] = Field(default="list", description="The type of object")
+    data: List[PydanticModelT] = Field(
+        default_factory=list, description="The data of the list"
+    )
+    next_page_token: Optional[Text] = Field(default=None, description="The token")
